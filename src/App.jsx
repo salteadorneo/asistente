769db3b711a8f23680da9173306d9cd9cbcc3883
Cristian Adán <cristian@doersdf.com>
2023-03-10 20:37:30 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Github from './components/Github'
 function App () {
   return (
     <>
-      <header className='fixed top-0 z-10 w-full flex justify-end p-4'>
+      <header className='fixed top-0 left-0 z-10 w-full flex justify-end p-4'>
         <nav className='flex items-center gap-4 text-sm'>
           <Link href='/'>Inicio</Link>
           <Link href='/directorio'>Directorio</Link>
