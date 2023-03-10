49ac16df0362e5d66a776d243509b91b82356364
Cristian Adán <cristian@doersdf.com>
2023-03-10 16:47:28 +0100

    add default message

--- api/chat.js
+++ api/chat.js
@@ -6,15 +6,22 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
+const DEFAULT_MESSAGE = 'No entiendo lo que quieres decir.'
+
 export default async function handler (req, res) {
   const { prompt } = req.query
 
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
-      { role: 'user', content: `Actúa como un asistente perezoso y borde que no explica bien: "${prompt}"` }
+      {
+        role: 'user',
+        content: `Actúa como un asistente perezoso y borde que no explica bien: "${prompt}"`
+      }
     ]
   })
 
-  res.status(200).json({ content: response.data.choices[0].message.content })
+  res.status(200).json({
+    content: response?.data?.choices?.[0]?.message?.content || DEFAULT_MESSAGE
+  })
 }
