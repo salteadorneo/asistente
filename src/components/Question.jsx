5ec33cee16f74b36acf75f641f2d1c4713751619
Cristian Adán <hola@salteadorneo.dev>
2023-03-12 21:59:31 +0100

    css dark fixes

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,22 +2,22 @@ import ShareButton from './ShareButton'
 
 export default function Question ({ question, answer }) {
   return (
-    <div className='w-full text-left'>
-      <div className='shadow-md bg-gray-10 rounded p-6 flex flex-col gap-4 justify-between leading-tight'>
-        <div>
-          <p class='text-gray-300 text-xs'>Pregunta</p>
-          <p className='text-gray-800 font-bold'>
-            {question}
-          </p>
-        </div>
-        <div>
-          <p class='text-gray-300 text-xs'>Respuesta</p>
-          <p>
-            {answer}
-          </p>
-        </div>
-        <ShareButton question={question} answer={answer} />
+    <div className='w-full text-left dark:bg-gray-100 shadow-md bg-gray-10 rounded p-6 flex flex-col gap-4 justify-between leading-tight'>
+      <div>
+        <p class='text-gray-300 dark:text-gray-700 text-xs'>Pregunta</p>
+        <p className='text-black font-bold'>
+          {question}
+        </p>
       </div>
+      <div>
+        <p class='text-gray-300 dark:text-gray-700 text-xs'>Respuesta</p>
+        <p class='dark:text-black'>
+          {answer}
+        </p>
+      </div>
+      <p className='dark:text-black'>
+        <ShareButton question={question} answer={answer} />
+      </p>
     </div>
   )
 }
