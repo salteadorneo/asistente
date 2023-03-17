c48fa165993cc8bc813f4f0f77f57735f695c5fb
Cristian Adán <cristian@doersdf.com>
2023-03-17 20:06:25 +0100

    fix classname

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,14 +4,14 @@ export default function Question ({ question, answer }) {
   return (
     <div className='w-full text-left dark:bg-gray-100 shadow-md bg-gray-10 rounded p-6 flex flex-col gap-4 justify-between leading-tight'>
       <div>
-        <p class='text-gray-300 dark:text-gray-700 text-xs'>Pregunta</p>
+        <p className='text-gray-300 dark:text-gray-700 text-xs'>Pregunta</p>
         <p className='text-black font-bold'>
           {question}
         </p>
       </div>
       <div>
-        <p class='text-gray-300 dark:text-gray-700 text-xs'>Respuesta</p>
-        <p class='dark:text-black'>
+        <p className='text-gray-300 dark:text-gray-700 text-xs'>Respuesta</p>
+        <p className='dark:text-black'>
           {answer}
         </p>
       </div>
